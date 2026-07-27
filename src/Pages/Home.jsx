bdd3abbdf69222eb1811083bc9dd12359e97f2b5
anujkumaryadav4169 <anujkumaryadav8649@gmail.com>
2026-07-27 14:41:28 +0530

    02:40

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import data from "../data/data";
+
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="home">
+
+      <h1>Welcome</h1>
+
+      <h2>Hi, I am {data.name}</h2>
+
+      <p>{data.shortBio}</p>
+
+      <div className="profile-card">
+        <img
+          src={data.image}
+          alt={data.name}
+          className="profile-image"
+        />
+      </div>
+
+      <button onClick={() => navigate("/about")}>
+        Know More
+      </button>
+
+    </div>
+  );
+}
+
+export default Home;
